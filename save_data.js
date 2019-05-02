5fe70445faf50a930a68dd48fcf7031d26338c4f
sagar-sehgal <sagar.r16@iiits.in>
2019-05-02 12:30:42 +0530

    comments added

--- save_data.js
+++ save_data.js
@@ -1,5 +1,9 @@
+// function to update the github id and save it in the storage
 function saveID(){
+	
 	var gitid=document.getElementById('gitid');
+
+	// storing the github id in the storage of extension 
 	chrome.storage.sync.set({'gitid': gitid.value},function(){
 		document.getElementById("gitimage").src="http://ghchart.rshah.org/"+gitid.value;
 		document.getElementById("gitname").innerHTML="@"+gitid.value;		
@@ -8,11 +12,13 @@ function saveID(){
 
 document.addEventListener('DOMContentLoaded', function() {
 	
+	// retrieving the saved github-id
 	chrome.storage.sync.get(['gitid'], function(result) {
 		document.getElementById('gitid').value=result.gitid;
 		saveID();
 	});
 
+	// adding event listener on pressing Enter Key
   	document.getElementById("gitid").addEventListener('keypress', function (e) {
     	var key = e.which || e.keyCode;
 	    if (key === 13) { // 13 is enter
